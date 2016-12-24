df9fa33cd605ddfae03e7be3de66cb2e16744ab9
WizardMarcos <wizardmarcos@gmail.com>
2016-12-24 02:34:15 +0000

    Added class to draw sprites in 3D (well, the existing SpriteBatch already does this, this is just for convinience).

---
+++ Source/SpriteBatch3D.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WM.Framework
+{
+    public class SpriteBatch3D
+    {
+        private SpriteBatch spriteBatch;
+
+        public SpriteBatch3D(SpriteBatch spriteBatch)
+        {
+            this.spriteBatch = spriteBatch;
+        }
+
+        public void Begin(SpriteSortMode sortMode = SpriteSortMode.BackToFront, BlendState blendState = null,
+            SamplerState samplerState = null, DepthStencilState depthStencilState = null,
+            RasterizerState rasterizerState = null, Effect effect = null, Matrix? transformMatrix = null)
+        {
+            spriteBatch.Begin(sortMode, blendState, samplerState, depthStencilState, rasterizerState, effect, transformMatrix);
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
